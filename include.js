88cd7ecabeed12bbd697d6f5b2842c57b43d07c6
amolinad22 <molina.dupuy.angel@gmail.com>
2025-10-05 23:13:51 -0300

    Implementar include.js con carga basica de HTML

--- include.js
+++ include.js
@@ -1,35 +1,19 @@
-function loadHTML(id, filename, callback) {
-    // 1. Pide el contenido del archivo (ej. footer.html)
+function loadHTML(id, filename) {
+    // 1. Pide el contenido del archivo
     fetch(filename)
     .then(response => response.text())
     .then(data => {
-        // 2. Pega el contenido dentro del elemento que tenga el 'id'
+        // 2. Pega el contenido
         document.getElementById(id).innerHTML = data;
-        
-        if (callback) { 
-            callback();
-        }
-
     })
-    // 3. Si hay un error, lo muestra en la consola del navegador
     .catch(error => console.error('Error al cargar el HTML:', error));
 }
 
 
 // Cuando la página está lista, llama a la función para cargar los módulos
 document.addEventListener('DOMContentLoaded', () => {
-    // Le decimos: Busca el elemento con ID y pega el contenido
+    // Carga de todos los componentes
     loadHTML('footer-placeholder', 'includes/footer.html');
     loadHTML('header-placeholder', 'includes/header.html');
-    
-    // CARGA DE CONTENIDO PRINCIPAL: SOLUCIÓN FINAL
-    loadHTML('content-placeholder', 'includes/content.html', () => { 
-        // Despues de pegar content.html, cargamos y ejecutamos app.js
-        import('./js/app.js')
-            .then(module => {
-                 module.initApp(); 
-            })
-            .catch(error => console.error('Error al cargar app.js:', error));
-    });
-
+    loadHTML('content-placeholder', 'includes/content.html');
 });
